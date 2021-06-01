e8df14ce1f5a4d2324901958753eb53b47613da2
lucasvaudey <lucasvaudey@gmail.com>
2021-06-01 23:14:47 +0200

    server and first entity creation

---
+++ server/src/entities/Users.ts
@@ -0,0 +1,36 @@
+import { Field, Int, ObjectType } from "type-graphql";
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@ObjectType()
+@Entity()
+export class Users extends BaseEntity {
+  @Field(() => Int)
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Field(() => String)
+  @CreateDateColumn()
+  createdAt = new Date();
+
+  @Field(() => String)
+  @UpdateDateColumn()
+  updatedAt = new Date();
+
+  @Field(() => String)
+  @Column({ unique: true })
+  username!: string;
+
+  @Field(() => String)
+  @Column({ unique: true })
+  email!: string;
+
+  @Column({ type: "text" })
+  password: string;
+}
---
+++ server/src/index.ts
@@ -0,0 +1,33 @@
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import { buildSchema } from "type-graphql";
+import { createConnection } from "typeorm";
+import { Users } from "./entities/Users";
+import { HelloResolver } from "./resolvers/hello";
+
+const main = async () => {
+  const conn = await createConnection({
+    type: "postgres",
+    database: "fridgeTime",
+    username: "lucas",
+    logging: true,
+    synchronize: true,
+    entities: [Users],
+  });
+  const app = express();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+    context: ({ req, res }) => ({ req, res }),
+  });
+  apolloServer.applyMiddleware({ app });
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
+};
+
+main().catch((err) => {
+  console.log(err);
+});
